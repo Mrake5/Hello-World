13e3792d4eb9d8a5955894c158795eebc640ab8b
Mrake5 <35434035+Mrake5@users.noreply.github.com>
2021-05-16 17:13:39 -0400

    Commit

--- Learning/Function_Shell.ps1
+++ Learning/Function_Shell.ps1
@@ -2,19 +2,23 @@ function New-AdvancedFunction {
     <#
     .SYNOPSIS
     .EXAMPLE
-        PS>
-    .PARAMETER
+        PS> New-AdvancedFunction -Param1 MYPARAM
+
+        This example does something to this and that.
+    .PARAMETER Param 1
+        This param does this thing.
     .PARAMETER
     .PARAMETER    
     #>
     [CmdletBinding()]
     param (
-
+        [string]$Param1
     )
     process {
         try {
             
         }
+## Good Default Practice to simplify error displays
         catch {
             Write-Error "$($_.Exception.Message) - Line Number: $($_.InvocationInfo.ScriptLineNumber)"
         }
